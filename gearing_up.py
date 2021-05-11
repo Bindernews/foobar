d501ce00369b1c97c3f481794f4f016d8e11d1dd
Andrew Heintz <bindernews@gmail.com>
2021-05-10 20:23:15 -0400

    Complete redesign using math instead of trying to iterate to the solution

--- gearing_up.py
+++ gearing_up.py
@@ -141,8 +141,42 @@ def solution1(pegs):
                 print("Stack down %d - %s" % (len(states), str(st.rg)))
 
 def solution(pegs):
-    return solution1(pegs)
+    diffs = [0] + [(pegs[i] - pegs[i - 1]) for i in range(1, len(pegs))]
 
+    def compute_radii(r0):
+        v = [r0]
+        for i in range(1, len(pegs)):
+            v.append(diffs[i] - v[i - 1])
+        return v
+    
+    r0 = Range(2, diffs[1])
+    # Constrain values so we shouldn't have any radii below 0
+    minM = min(compute_radii(r0.min))
+    maxM = min(compute_radii(r0.max))
+    if minM < 1:
+        r0.min -= (minM)
+    if maxM < 1:
+        r0.max += (maxM)
+    # Compute new ending radii
+    r1 = Range(compute_radii(r0.min)[-1], compute_radii(r0.max)[-1])
+    # Solve for value where 0th radius = 2*nth radius
+    # This becomes a line, as 0th radius goes up/down (depending on if there are an even or odd number of gears)
+    # the nth radius goes up at half the speed. "0.5x = mx + b" => "(0.5 - m)x = b" => "x = b / (0.5 - m)"
+    # m simply determines if there are an even or odd number of gears  m = (r1.max - r1.min) / (r0.max - r0.min)
+    if len(pegs) % 2 == 1:
+        m = 1
+    else:
+        m = -1
+    b = r1.min - (m * r0.min)
+    x = b / (0.5 - m)
+    #print("m = %f  b = %f  x = %f" % (m, b, x))
+    # Finally we test to make sure there are no invalid radii
+    for r in compute_radii(x):
+        if r < 1:
+            return (-1, -1)
+    return x.as_integer_ratio()
+    
+    
 def make_test(sln, peg0):
     pegs = [peg0]
     for i in range(1, len(sln)):
